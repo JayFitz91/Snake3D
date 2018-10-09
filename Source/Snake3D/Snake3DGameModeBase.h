7cb361c9b02163769ac1fd3af03069c2f502a45a
DESKTOP-JRAT89P\Fitze <jayfitz91@gmail.com>
2018-10-09 20:33:26 +0100

    Initial Commit

---
+++ Source/Snake3D/Snake3DGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Snake3DGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SNAKE3D_API ASnake3DGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
